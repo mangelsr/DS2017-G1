a90ed2cb7acf3292683473fce20b0365ba4051ef
Miguel Sanchez <mangelsr@live.com>
2017-05-28 17:35:39 -0500

    [ADD] custom login que verifica el rol del usuario, [ADD] pagina de logout

--- Catalogo_de_Delicias/catalogoapp/urls.py
+++ Catalogo_de_Delicias/catalogoapp/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import include, url
-from . import views
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
+from .views import *
 
+from . import views
 urlpatterns = [
-    url(r'^login/$',views.login),
+    url(r'^$', login),
+    url(r'^login/$', login),
+    url(r'^logout/$', logout),
 ]
--- Catalogo_de_Delicias/catalogoapp/views.py
+++ Catalogo_de_Delicias/catalogoapp/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+from .models import Perfil
 
 # Create your views here.
 
 def login(request):
-    return render(request,'login.html',{})
+    if (request.method == 'POST'):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            rol = Perfil.objects.get(user=user).rol
+            if (rol == 'Cliente'):
+                return redirect('../homeCliente')
+            elif (rol == 'Ayudante'):
+                return redirect('../homeAyudante')
+            else:
+                return redirect('../homeAdmin')
+        else:
+            print("ERROR DE AUTENTICACION...")
+            response = HttpResponse("ERROR DE AUTENTICACION...",status=500)
+            return response
+    else:
+        return render(request,'login.html',{})
+
+def logout(request):
+    auth_logout(request)
+    return render(request,'logout.html',{})
