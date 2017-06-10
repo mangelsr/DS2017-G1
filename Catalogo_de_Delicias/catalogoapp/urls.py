3882fdccfe867936bf0a3828c4e6f2b580686dfc
Miguel Sanchez <mangelsr@live.com>
2017-06-10 14:36:31 -0500

    [ADD] funciones de view Ayudante

--- Catalogo_de_Delicias/catalogoapp/urls.py
+++ Catalogo_de_Delicias/catalogoapp/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     url(r'^homeAssistant/$', homeAssistant, name='homeAssistant'),
     url(r'^homeAssistant/listDishes$', listDishesAssitant, name='listDishesAssitant'),
     url(r'^homeAssistant/listCategoryDishes$', listCategoryDishesAssitant, name='listCategoryDishesAssitant'),
-    url(r'^homeAssistant/newDish$', new_dish, name='homeDish'),
+    url(r'^homeAssistant/newDish$', new_dish, name='newDish'),
 
     url(r'^homeAdmin/$', homeAdmin, name='homeAdmin'),
 ]
