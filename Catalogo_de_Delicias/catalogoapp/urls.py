7bd14a4dd2741b236316f337701939e7694cfc2f
Miguel Sanchez <mangelsr@live.com>
2017-06-11 12:02:33 -0500

    [ADD] autenticacion y verificacion de permisos en las vistas de funciones del sistema, [ADD] breve documentacion de urls y vistas

--- Catalogo_de_Delicias/catalogoapp/urls.py
+++ Catalogo_de_Delicias/catalogoapp/urls.py
@@ -7,21 +7,26 @@ from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
+    #URLS DE INCIO/CIERRE/PERMISOS
     url(r'^$', login, name="login"),
     url(r'^login/$', login),
     url(r'^logout/$', logout, name="logout"),
+    url(r'^noAccess/$', noAccess, name="noAccess"),
 
+    #URLS DEL ROL CLIENTE
     url(r'^homeClient/$', homeClient, name='homeCliente'),
     url(r'^homeClient/listDishes$', listDishes, name='listDishes'),
     url(r'^homeClient/searchDishes$', searchDishes, name='searchDishes'),
     url(r'^homeClient/viewDish/(?P<id_dish>\d+)/$',viewDish, name='viewDish'),
 
+    #URLS DEL ROL AYUDANTE/ASISTENTE
     url(r'^homeAssistant/$', homeAssistant, name='homeAssistant'),
     url(r'^homeAssistant/listDishes$', listDishesAssitant, name='listDishesAssitant'),
     url(r'^homeAssistant/listCategoryDishes$', listCategoryDishes, name='listCategoryDishes'),
     url(r'^homeAssistant/newDish$', new_dish, name='newDish'),
     url(r'^homeAssistant/viewDish/(?P<id_dish>\d+)/$',assistantviewDish, name='assistantviewDish'),
 
+    #URLS DEL ROL ADMINISTRADOR
     url(r'^homeAdmin/$', homeAdmin, name='homeAdmin'),
     url(r'^homeAdmin/newRestaurant$', newRestaurant, name='newRestaurant'),
     url(r'^homeAdmin/listRestaurant$', listRestaurant, name='listRestaurant'),
--- Catalogo_de_Delicias/catalogoapp/views.py
+++ Catalogo_de_Delicias/catalogoapp/views.py
@@ -33,127 +33,166 @@ def logout(request):
     auth_logout(request)
     return render(request,'logout.html',{})
 
+def noAccess(request):
+    return render(request, 'noAccess.html', {})
+
+
+#VISTAS DE HOME PARA CADA ROL
 def homeClient(request):
-    if request.user.is_authenticated:
+    if (request.user.is_authenticated and (request.user.profile.role == "Cliente")):
         return render(request,'homeClient.html')
     else:
-        print("DEBE ESTAR LOGEADO...")
-        return redirect('../')
+        return redirect('noAccess')
 
 def homeAssistant(request):
-    if request.user.is_authenticated:
+    if (request.user.is_authenticated and (request.user.profile.role == "Ayudante")):
         return render(request,'homeAssistant.html')
     else:
-        print("DEBE ESTAR LOGEADO...")
-        return redirect('../')
+        return redirect('noAccess')
 
 def homeAdmin(request):
-    if request.user.is_authenticated:
+    if (request.user.is_authenticated and (request.user.profile.role == "Administrador")):
         return render(request,'homeAdmin.html')
     else:
-        print("DEBE ESTAR LOGEADO...")
-        return redirect('../')
+        return redirect('noAccess')
 
+#VISTAS PARA EL ROL DE CLIENTE
 def listDishes(request):
-    if (request.method == "POST"):
-        naperitivos = len(Dish.objects.filter(dish_choice='Aperitivo'))
-        nfuertes = len(Dish.objects.filter(dish_choice='Plato Fuerte'))
-        npostres = len(Dish.objects.filter(dish_choice='Postre'))
-        selection = request.POST['selection']
-        dishes = Dish.objects.filter(dish_choice=selection)
-        ndishes = len(dishes)
-        return render(request,'listDishes.html',{"ndishes":ndishes,"dishes":dishes,
-        "naperitivos":naperitivos,"nfuertes":nfuertes,"npostres":npostres})
+    if (request.user.is_authenticated and (request.user.profile.role == "Cliente")):
+        if (request.method == "POST"):
+            naperitivos = len(Dish.objects.filter(dish_choice='Aperitivo'))
+            nfuertes = len(Dish.objects.filter(dish_choice='Plato Fuerte'))
+            npostres = len(Dish.objects.filter(dish_choice='Postre'))
+            selection = request.POST['selection']
+            dishes = Dish.objects.filter(dish_choice=selection)
+            ndishes = len(dishes)
+            return render(request,'listDishes.html',{"ndishes":ndishes,"dishes":dishes,
+            "naperitivos":naperitivos,"nfuertes":nfuertes,"npostres":npostres})
+        else:
+            return render(request,'listDishes.html',{})
     else:
-        return render(request,'listDishes.html',{})
+        return redirect('noAccess')
 
 def searchDishes(request):
-    if (request.method == "POST"):
-        dishName = request.POST['dishName']
-        dishes = Dish.objects.filter(name__contains=dishName, description__contains=dishName)
-        ndishes = len(dishes)
-        return render(request,'searchDishes.html',{"ndishes":ndishes,"dishes":dishes})
+    if (request.user.is_authenticated and (request.user.profile.role == "Cliente")):
+        if (request.method == "POST"):
+            dishName = request.POST['dishName']
+            dishes = Dish.objects.filter(name__contains=dishName, description__contains=dishName)
+            ndishes = len(dishes)
+            return render(request,'searchDishes.html',{"ndishes":ndishes,"dishes":dishes})
+        else:
+            return render(request,'searchDishes.html',{})
     else:
-        return render(request,'searchDishes.html',{})
+        return redirect('noAccess')
 
 def viewDish(request,id_dish):
-    dish = Dish.objects.get(id = id_dish)
-    return render(request,'detailDish.html',{"dish":dish})
+    if (request.user.is_authenticated and (request.user.profile.role == "Cliente")):
+        dish = Dish.objects.get(id = id_dish)
+        return render(request,'detailDish.html',{"dish":dish})
+    else:
+        return redirect('noAccess')
 
+
+#VISTAS PARA EL ROL DE AYUDANTE/ASISTENTE DE RESTAURANTE
 def listDishesAssitant(request):
-    user = request.user
-    restaurant = Profile.objects.get(user=user).restaurant
-    dishes = Dish.objects.filter(restaurant=restaurant)
-    ndishes = len(dishes)
-    return render(request,'listDishesAssitant.html',{"restaurant":restaurant,"ndishes":ndishes,"dishes":dishes})
-
-def listCategoryDishes(request):
-    if (request.method == "POST"):
-        selection = request.POST['selection']
+    if (request.user.is_authenticated and (request.user.profile.role == "Ayudante")):
         user = request.user
         restaurant = Profile.objects.get(user=user).restaurant
-        dishes = Dish.objects.filter(restaurant=restaurant, dish_choice=selection)
+        dishes = Dish.objects.filter(restaurant=restaurant)
         ndishes = len(dishes)
-        return render(request, 'listCategoryDishes.html', {"restaurant":restaurant,"ndishes":ndishes,"dishes":dishes})
+        return render(request,'listDishesAssitant.html',{"restaurant":restaurant,"ndishes":ndishes,"dishes":dishes})
     else:
-        return render(request, 'listCategoryDishes.html', {})
+        return redirect('noAccess')
+
+def listCategoryDishes(request):
+    if (request.user.is_authenticated and (request.user.profile.role == "Ayudante")):
+        if (request.method == "POST"):
+            selection = request.POST['selection']
+            user = request.user
+            restaurant = Profile.objects.get(user=user).restaurant
+            dishes = Dish.objects.filter(restaurant=restaurant, dish_choice=selection)
+            ndishes = len(dishes)
+            return render(request, 'listCategoryDishes.html', {"restaurant":restaurant,"ndishes":ndishes,"dishes":dishes})
+        else:
+            return render(request, 'listCategoryDishes.html', {})
+    else:
+        return redirect('noAccess')
 
 def assistantviewDish(request,id_dish):
-    dish = Dish.objects.get(id = id_dish)
-    return render(request,'detailDishAssistant.html',{"dish":dish})
+    if (request.user.is_authenticated and (request.user.profile.role == "Ayudante")):
+        dish = Dish.objects.get(id = id_dish)
+        return render(request,'detailDishAssistant.html',{"dish":dish})
+    else:
+        return redirect('noAccess')
 
 def new_dish(request):
-    if request.method == 'POST':
-        user = request.user
-        form = DishForm(request.POST)
-        if form.is_valid():
-            newDish = form.save(commit=False)
-            newDish.restaurant = Profile.objects.get(user=user).restaurant
-            newDish.save()
-        return redirect('homeAssistant')
+    if (request.user.is_authenticated and (request.user.profile.role == "Ayudante")):
+        if request.method == 'POST':
+            user = request.user
+            form = DishForm(request.POST)
+            if form.is_valid():
+                newDish = form.save(commit=False)
+                newDish.restaurant = Profile.objects.get(user=user).restaurant
+                newDish.save()
+            return redirect('homeAssistant')
+        else:
+            form = DishForm()
+        return render(request,'dish_entry.html',{"dish":form})
     else:
-        form = DishForm()
-    return render(request,'dish_entry.html',{"dish":form})
+        return redirect('noAccess')
 
 def edit_dish(request,id_dish):
-    dish = Dish.objects.get(id = id_dish)
-    if request.method == "GET":
-        form = DishForm(instance=dish)
+    if (request.user.is_authenticated and (request.user.profile.role == "Ayudante")):
+        dish = Dish.objects.get(id = id_dish)
+        if request.method == "GET":
+            form = DishForm(instance=dish)
+        else:
+            form = DishForm(request.POST,instance=dish)
+            if form.is_valid():
+                form.save()
+            return redirect('homeAssistant')
+        return render(request,'dish_entry.html',{"dish":form})
     else:
-        form = DishForm(request.POST,instance=dish)
-        if form.is_valid():
-            form.save()
-        return redirect('homeAssistant')
-    return render(request,'dish_entry.html',{"dish":form})
+        return redirect('noAccess')
 
+#VISTAS PARA EL ROL DE ADMINISTRADOR
 def newRestaurant(request):
-    if request.method == 'POST':
-        form = RestaurantForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('homeAdmin')
+    if (request.user.is_authenticated and (request.user.profile.role == "Administrador")):
+        if request.method == 'POST':
+            form = RestaurantForm(request.POST)
+            if form.is_valid():
+                form.save()
+            return redirect('homeAdmin')
+        else:
+            form = RestaurantForm()
+        return render(request, 'newRestaurant.html',{'form': form})
     else:
-        form = RestaurantForm()
-    return render(request, 'newRestaurant.html',{'form': form})
+        return redirect('noAccess')
 
 def listRestaurant(request):
-    restaurantes = Restaurant.objects.all()
-    asistentes = Profile.objects.filter(role="Ayudante")
-    platillos = Dish.objects.all()
-    return render(request, 'listRestaurant.html',{'restaurantes':restaurantes,
-    'asistentes':asistentes,'platillos':platillos})
+    if (request.user.is_authenticated and (request.user.profile.role == "Administrador")):
+        restaurantes = Restaurant.objects.all()
+        asistentes = Profile.objects.filter(role="Ayudante")
+        platillos = Dish.objects.all()
+        return render(request, 'listRestaurant.html',{'restaurantes':restaurantes,
+        'asistentes':asistentes,'platillos':platillos})
+    else:
+        return redirect('noAccess')
 
 def newUser(request):
-    if request.method == 'POST':
-        newUser = UserForm(request.POST)
-        newProfile = ProfileForm(request.POST)
-        if newUser.is_valid() and newProfile.is_valid():
-            user = newUser.save()
-            profile = newProfile.save(commit=False)
-            profile.user = user
-            profile.save()
-        return redirect('homeAdmin')
+    if (request.user.is_authenticated and (request.user.profile.role == "Administrador")):
+        if request.method == 'POST':
+            newUser = UserForm(request.POST)
+            newProfile = ProfileForm(request.POST)
+            if newUser.is_valid() and newProfile.is_valid():
+                user = newUser.save()
+                profile = newProfile.save(commit=False)
+                profile.user = user
+                profile.save()
+            return redirect('homeAdmin')
+        else:
+            newUser = UserForm()
+            newProfile = ProfileForm()
+        return render(request,'newUser.html',{'user':newUser,'profile':newProfile})
     else:
-        newUser = UserForm()
-        newProfile = ProfileForm()
-    return render(request,'newUser.html',{'user':newUser,'profile':newProfile})
+        return redirect('noAccess')
