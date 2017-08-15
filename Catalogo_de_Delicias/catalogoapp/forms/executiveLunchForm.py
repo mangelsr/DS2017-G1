15dd5637592426a528a89a404b46b12d99e2a9ca
Alexis <alexisleo15@gmail.com>
2017-08-15 16:30:06 -0500

    [ ADD] executiveLunchForm

--- Catalogo_de_Delicias/catalogoapp/forms/executiveLunchForm.py
+++ Catalogo_de_Delicias/catalogoapp/forms/executiveLunchForm.py
@@ -1,24 +1,29 @@
 from django import forms
+from catalogoapp.models.executiveLunch import ExecutiveLunch
 
-from catalogoapp.models.lunch import Lunch
 
-
-class LunchForm(forms.ModelForm):
+class ExecutiveLunchForm(forms.ModelForm):
 
     class Meta:
-        model = Lunch
+        model =  ExecutiveLunch
         fields = [
             'soup',
             'main_curse',
             'date',
+            'dessert',
+            'juice',
         ]
         labels = {
             'soup': 'Sopa del almuerzo',
             'main_curse': 'Plato fuerte del almuerzo',
             'date': 'Fecha del almuerzo',
+            'dessert': 'Postre del almuerzo',
+            'juice': 'Jugo del postre',
         }
         widgets = {
             'soup': forms.Select(attrs={'class': 'form-control', 'id': 'soup'}),
             'main_curse': forms.Select(attrs={'class': 'form-control', 'id': 'main_course'}),
             'date': forms.DateInput(),
+            'dessert': forms.Select(attrs={'class': 'form-control', 'id': 'dessert'}),
+            'juice': forms.Select(attrs={'class': 'form-control', 'id': 'juice'}),
         }
