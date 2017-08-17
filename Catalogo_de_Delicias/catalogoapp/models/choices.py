b435517bd6b2049d3a40a06b8f7495cb142ca21d
Viviana Laurido <vlaurido@espol.edu.ec>
2017-08-16 19:07:13 -0500

    cambios

---
+++ Catalogo_de_Delicias/catalogoapp/models/choices.py
@@ -0,0 +1,15 @@
+
+fonts = (
+    ("Arial", "Arial"),
+    ("Helvetica", "Helvetica"),
+    ("Gadget", "Gadget"),
+    ("Comic Sans MS", "Comic Sans MS"),
+    ("Impact", "Impact"),
+    ("Charcoal", "Charcoal"),
+    ("Tahoma", "Tahoma"),
+    ("Helvetica", "Helvetica"),
+    ("Trebuchet MS", "Trebuchet MS"),
+    ("Courier New", "Courier New"),
+    ("Lucida Console", "Lucida Console"),
+    ("Monaco", "Monaco"),
+  )
