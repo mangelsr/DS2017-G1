2807f3d91f996c8b00eb1eaf01a7eceb381c6095
Miguel Sanchez <mangelsr@live.com>
2017-08-18 19:37:35 -0500

    [UPD] Se actulizo el template baseform2 para que oculte las opciones que depende del rol del usuario a ingresar

--- Catalogo_de_Delicias/catalogoapp/models/profile.py
+++ Catalogo_de_Delicias/catalogoapp/models/profile.py
@@ -8,8 +8,8 @@ class Profile(models.Model):
     user = models.OneToOneField(
         User, primary_key=True, related_name='profile', on_delete=models.CASCADE)
     role = models.ForeignKey(Role, null=False, blank=False)
-    restaurant = models.ForeignKey(Restaurant, related_name='profile_set', null=True, blank=True,
-                                   help_text='Solo necesario si el rol es Ayudante', related_query_name="profiles")
+    restaurant = models.ForeignKey(Restaurant, related_name='profile_set', null=True, blank=True, 
+                                    related_query_name="profiles")
     is_student = models.BooleanField()
 
     def __unicode__(self):
