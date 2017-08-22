3d3535d2a740ae8e3561e1f161780d6c7a046e1a
Viviana Laurido <vlaurido@espol.edu.ec>
2017-08-21 21:58:28 -0500

    Cambios pruebas unitarias... no sale :(  se edite readme con el comando para probar

--- Catalogo_de_Delicias/catalogoapp/tests/orderTest.py
+++ Catalogo_de_Delicias/catalogoapp/tests/orderTest.py
@@ -11,7 +11,6 @@ class OrderTestCase(TestCase):
     #Prueba calculando el total esperado de un almuerzo estudiantil
     def test_total_lunchEstudiantil(self):
         orders = Order.objects.filter(include_juice=False,include_dessert=False)
-        lunch = None
         for order in orders:
             if not hasattr(order.lunch,"executivelunch"):
                 lunch = order
@@ -21,24 +20,26 @@ class OrderTestCase(TestCase):
     #Prueba calculando el total esperado de un almuerzo ejecutivo
     def test_total_lunchEjecutivo(self):
         orders = Order.objects.filter(include_juice=False,include_dessert=False)
-        executiveLunch = None
         for order in orders:
             if hasattr(order.lunch,"executivelunch"):
-                executivelunch = order
+                executivelunch = Order(order)
 
         self.assertEqual(executiveLunch.calculateCost(executiveLunch.lunch, False, False), 3.00)
 
     #Prueba calculando el total esperado de un almuerzo ejecutivo con bebida
     def test_total_lunchEjecutivo_bebida(self):
         order = Order.objects.filter(include_juice=True)[0]
+        print(order)
         self.assertEqual(order.calculateCost(order.lunch, False, True), 3.50)
 
     #Prueba calculando el total esperado de un almuerzo ejecutivo con postre
     def test_total_lunchEjecutivo_postre(self):
         order = Order.objects.filter(include_dessert=True)[0]
+        print(order)
         self.assertEqual(order.calculateCost(order.lunch, True, False), 3.75)
 
     #Prueba calculando el total esperado de un almuerzo ejecutivo con bebida y postre
     def test_total_lunchEjecutivo_ambos(self):
-        order = Order.objects.self.filter(include_juice=True,include_dessert=True)[0]
+        order = Order.objects.filter(include_juice=True,include_dessert=True)[0]
+        print(order)
         self.assertEqual(order.calculateCost(order.lunch, True, True), 4.25)
